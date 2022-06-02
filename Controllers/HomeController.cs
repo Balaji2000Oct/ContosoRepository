c5223d9b84e2e488d7ff8b8e6ca20fe4e3fc34c7
Balaji G <Balaji.G@kryptostech.com>
2022-06-02 08:48:37 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Contoso_University.Models;
+using ContosoUniversity.Models;
+using Microsoft.Extensions.Logging;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contoso_University.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
