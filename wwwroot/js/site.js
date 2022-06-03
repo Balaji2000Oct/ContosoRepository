c63f65a224a03db1aef907cf73d97b05febe4f3e
Balaji G <Balaji.G@kryptostech.com>
2022-06-03 10:18:40 +0530

    m

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,17 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets
 // Write your JavaScrip
-function sample1(s) {
-    const colarr = ["red", "green", "black", "yellow", "gold", "darkorange"];
-    console.log("Array before : " + colarr);
-    console.log("Array Size"+colarr.length);
-    colarr.pop(s);
-    console.log("Array after : " + colarr);
-    console.log("Array Size" + colarr.length);
-    var n = Math.floor(Math.random(0, colarr.length - 1));
-    console.log("Random number " + n);
-    document.getElementById("samp").style.color = colarr[n];
-}
+setInterval(
+    function sample1(s) {
+        //const colarr = ["red", "green", "black", "crimson", "gold", "darkorange"];
+        //console.log("Array before : " + colarr);
+        //console.log("Array Size" + colarr.length);
+        //colarr.pop(s);
+        //console.log("Array after : " + colarr);
+        //console.log("Array Size" + colarr.length);
+        var n = Math.floor(Math.random() * 16777215);
+        console.log(" Random number " + n.toString(16));
+        document.getElementById("samp").style.color ="#" +n.toString(16);
+    }, 2000
+);
+ser
